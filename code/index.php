8b82514a7c9fdeabe66c01c8539f12e9f02f0822
yokada2254 <chankwonghei@hotmail.com>
2020-09-19 01:07:29 +0800

    update

--- code/index.php
+++ code/index.php
@@ -1,5 +1,12 @@
 <?php
 #phpinfo();
 
-echo function_exists('mysqli_connect')?'1':'2';
+#echo function_exists('mysqli_connect')?'1':'2';
+$link = mysqli_connect("mysql", "root", "1234")or die(mysqli_connect_error());
 
+$sql = "SHOW DATABASES;";
+$res = mysqli_query($link, $sql);
+while($r = mysqli_fetch_assoc($res)){
+    echo print_r($r, 1);
+}
+?>
